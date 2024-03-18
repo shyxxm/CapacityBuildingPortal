1432be0bf74fbd1760f8ec7486ca4ea49990d158
Shyam Prasad <48471694+shyxxm@users.noreply.github.com>
2024-03-18 21:40:53 +0800

    project details

--- react-frontend/src/pages/ProjectDetails.js
+++ react-frontend/src/pages/ProjectDetails.js
@@ -7,16 +7,22 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
 
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import dayjs from "dayjs";
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const names = ["Swamiji", "Swami P", "Swami AA", "Swami PVS", "Swami AG"];
 
 const okrs = ["OKR1", "OKR2", "OKR3"];
+
+const KPIs = ["KPI1", "KPI2", "KPI3"];
+
+const Progress = ["Red", "Yellow", "Green"];
+
+
 
 const handleNotificationClick = () => {
   // Add your functionality here
@@ -40,9 +46,16 @@ function ProjectDetails() {
     setselectedOKR(event.target.value);
   };
 
-  const handleDateChange = (newDate) => {
-    setSelectedYear(newDate);
-    console.log(newDate);
+  const [selectedKPI, setselectedKPI] = React.useState("");
+
+  const handleKPIChange = (event) => {
+    setselectedKPI(event.target.value);
+  };
+
+  const [selectedProgress, setselectedProgress] = React.useState("");
+
+  const handleKPIProgressChange = (event) => {
+    setselectedKPI(event.target.value);
   };
 
   return (
@@ -101,7 +114,7 @@ function ProjectDetails() {
                 <div className="card">
                   <div className="card-body">
                     <h5 className="card-title fw-semibold mb-4">
-                      Edit Project Details
+                      OKR Project Details
                     </h5>
                     <div style={{ display: "flex", alignItems: "center" }}>
                       <FormControl sx={{ mt: 1 }}>
@@ -125,17 +138,80 @@ function ProjectDetails() {
                       </FormControl>
 
                       <div style={{ marginLeft: "48px" }}>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                          <DemoContainer components={["DatePicker"]}>
-                            <DatePicker
-                              label="OKR year"
-                              views={["year"]}
-                              value={selectedYear}
-                              onChange={handleDateChange}
-                            />
-                          </DemoContainer>
-                        </LocalizationProvider>
+                        <TextField
+                          id="outlined-required"
+                          label="OKR Date"
+                          color="secondary"
+                          name="projectName"
+                          sx={{ width: 230 }} // Adjust the width as needed
+                        />
                       </div>
+                      <div style={{ marginLeft: "48px" }}>
+
+                      <TextField
+                          id="outlined-required"
+                          label="OKR Quarter Range"
+                          color="secondary"
+                          name="projectName"
+                          sx={{ width: 230 }} // Adjust the width as needed
+                        />
+                                              </div>
+
+                    </div>
+                  </div>
+                </div>
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title fw-semibold mb-4">
+                      KPI Project Details
+                    </h5>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <FormControl sx={{ mt: 1 }}>
+                        <InputLabel id="demo-simple-select-label">
+                          KPI
+                        </InputLabel>
+                        <Select
+                          labelId="demo-simple-select-label"
+                          id="demo-simple-select"
+                          value={selectedKPI}
+                          label="KPI"
+                          onChange={handleKPIChange}
+                          sx={{ width: 200 }}
+                        >
+                          {KPIs.map((KPIs) => (
+                            <MenuItem key={KPIs} value={KPIs}>
+                              {KPIs}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+                      <div style={{ marginLeft:48 }}>
+                      <FormControl sx={{ mt: 1 }}>
+                        <InputLabel id="demo-simple-select-label">
+                          Progress
+                        </InputLabel>
+                        <Select
+                          labelId="demo-simple-select-label"
+                          id="demo-simple-select"
+                          value={selectedProgress}
+                          label="Progress"
+                          onChange={handleKPIProgressChange}
+                          sx={{ width: 200 }}
+                        >
+                          {Progress.map((Progress) => (
+                            <MenuItem key={Progress} value={Progress}>
+                              {Progress}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+                      </div>
+
+                      <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DemoContainer components={['DatePicker']}>
+                          <DatePicker label="KPI date" />
+                        </DemoContainer>
+                      </LocalizationProvider>
                     </div>
                   </div>
                 </div>
